602cef3684a59d0b74b8a2fc56f9fc050d653fcf
Alice <alicezou05@gmail.com>
2018-10-07 20:13:32 -0400

    Updated the test cases for the app.component.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularn'`, () => {
+  it(`should have as title 'My First Angular7/Next App!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angularn');
+    expect(app.title).toEqual('My First Angular7/Next App!');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularn!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to My First Angular7/Next App!!');
   });
 });
